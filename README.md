adbc2aebfc28d5a5d209cac9b789347af54aed47
Moonflower2022 <harqian@nuevaschool.org>
2024-09-01 15:17:39 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # text-generation
+
 A collection of text generation algorithms trained on different text sources like movie scripts, plays, etc.
+
+## text sources (in /texts)
+
+* `peter_pan.txt`: <https://www.gutenberg.org/files/16/16-h/16-h.htm#chap17>
+* `shakespeare.txt`: <https://www.gutenberg.org/ebooks/100>
+* `bee_movie.txt`: <https://courses.cs.washington.edu/courses/cse163/20wi/files/lectures/L04/bee-movie.txt>
+
+## generation algorithms
+
+* [LSTM (Long Short-term Memory)](https://en.wikipedia.org/wiki/Long_short-term_memory): `train_lstm.ipynb`
+* [GRU (Gated Recurrent Unit)](https://en.wikipedia.org/wiki/Gated_recurrent_unit): `train_gru.ipynb`
+* [Markov Chain](https://en.wikipedia.org/wiki/Markov_chain): `markov_chain.py`
---
+++ markov_chain.py
@@ -0,0 +1,61 @@
+import random
+import string
+import os
+
+def create_chain(training_data):
+    markov_chain = {}
+
+    for sequence, next in training_data:
+        if sequence in markov_chain:
+            markov_chain[sequence] += [next]
+        else: # not sequence in markov_chain
+            markov_chain[sequence] = [next]
+
+    return markov_chain
+
+def get_result_from_chain(markov_chain, sequence):
+    if sequence in markov_chain:
+        return random.choice(markov_chain[sequence])
+    else:
+        print(f"unknown sequence {sequence}")
+        return random.choice(string.ascii_letters)
+    
+def generate_text():
+    with open(f"texts/{TEXT_NAME}.txt", "r") as file:
+        text = file.read()
+
+    print("# of characters:", len(text))
+
+    sequences = []
+
+    for i in range(len(text) - SEQUENCE_LENGTH):
+        sequences.append((text[i : i + SEQUENCE_LENGTH], text[i + SEQUENCE_LENGTH]))
+
+    markov_chain = create_chain(sequences)
+
+    generated_text = random.choice(list(markov_chain.keys())) # "".join([random.choice(text) for _ in range(SEQUENCE_LENGTH)])
+
+    for i in range(NUM_GENERATED_CHARACTERS):
+        generated_text += get_result_from_chain(markov_chain, generated_text[-SEQUENCE_LENGTH:])
+
+    print(generated_text)
+
+    return generated_text
+
+def get_avaialable_file_name(file_name, extension):
+    available_file_name = file_name
+    i = 1
+    while os.path.isfile(available_file_name + extension):
+        available_file_name = file_name + f"_{i}"
+        i += 1
+
+    return available_file_name + extension
+
+if __name__ == '__main__':
+    SEQUENCE_LENGTH = 5
+    TEXT_NAME = "shakespeare"
+    NUM_GENERATED_CHARACTERS = 1000
+    generated_text = generate_text()
+
+    with open(get_avaialable_file_name(f"generated_texts/{TEXT_NAME}_markov_chain_{SEQUENCE_LENGTH}chars", ".txt"), "w") as file:
+        file.write(generated_text)
